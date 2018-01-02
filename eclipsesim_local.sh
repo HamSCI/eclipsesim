c3411e20c42248afae4341e039318b6cf7796b0e
Nathaniel A. Frissell <nathaniel.a.frissell@njit.edu>
2018-01-02 16:34:12 -0500

    job_name to job_id converter

--- eclipsesim_local.sh
+++ eclipsesim_local.sh
@@ -6,15 +6,6 @@ PHARLAP_DIR="${CWD}/pharlap_4.1.3/"
 export DIR_MODELS_REF_DAT="${PHARLAP_DIR}/dat"
 export MATLABPATH=`find ${PHARLAP_DIR} -type d | sort | sed ':a;N;$!ba;s/\n/:/g'`
 
-# Find our jobs
-jobs=()
-for entry in "./jobs"/*; do jobs+=("$entry"); done
-
-# Sort the jobs
-IFS=$'\n'
-jobs=($(sort <<<"${jobs[*]}"))
-unset IFS
-
 # Environment variables
 PLOTS=1
 ECLIPSE=1
@@ -25,6 +16,15 @@ SAMI3_PATH="./sami3/"
 OUT_PATH="./traces/"
 PLOT_PATH="./plots/"
 
-# Run the program
-matlab -nodisplay -r "dbstop in eclipse at 92; eclipse(${jobs[0]}, ${PLOTS}, ${ECLIPSE}, '${JOB_PATH}', '${OUT_PATH}', '${PLOT_PATH}', '${SAMI3_PATH}'); exit;"
-#matlab -nodisplay -r "eclipse(${jobs[0]}, ${PLOTS}, ${ECLIPSE}, '${JOB_PATH}', '${OUT_PATH}', '${PLOT_PATH}', '${SAMI3_PATH}'); exit;"
+# Find our jobs
+jobs=`ls -1 ${JOB_PATH}`
+## Sort the jobs
+IFS=$'\n'
+for this_job in ${jobs}; do
+    job_id=`./job_to_jobid "${this_job}"`
+
+    # Run the program
+#    matlab -nodisplay -r "dbstop in eclipse at 199; eclipse(${job_id}, ${PLOTS}, ${ECLIPSE}, '${JOB_PATH}', '${OUT_PATH}', '${PLOT_PATH}', '${SAMI3_PATH}'); exit;"
+    matlab -nodisplay -r "eclipse(${job_id}, ${PLOTS}, ${ECLIPSE}, '${JOB_PATH}', '${OUT_PATH}', '${PLOT_PATH}', '${SAMI3_PATH}'); exit;"
+done
+unset IFS
